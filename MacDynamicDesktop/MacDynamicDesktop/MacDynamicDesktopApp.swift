0f5ae2541d3f19ba102f1f7e9afd73250e1a75bd
Lars Krämer <lars.kraemer@live.de>
2023-07-15 23:49:56 +0200

    MDD-1: default project

---
+++ MacDynamicDesktop/MacDynamicDesktop/MacDynamicDesktopApp.swift
@@ -0,0 +1,20 @@
+//
+//  MacDynamicDesktopApp.swift
+//  MacDynamicDesktop
+//
+//  Created by Lars Krämer on 15.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MacDynamicDesktopApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
